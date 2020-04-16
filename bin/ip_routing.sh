e9e5b71046c3110d5a5d9265376e05d505f868bf
root <root@vps222215.vps.ovh.ca>
2020-04-16 12:23:58 -0400

    add new routing for grafana

--- bin/ip_routing.sh
+++ bin/ip_routing.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
 iptables -t nat -A PREROUTING -p tcp --dport 65010 -j DNAT --to-destination 192.168.255.10:80
+iptables -t nat -A PREROUTING -p tcp --dport 64010 -j DNAT --to-destination 192.168.255.10:3080
 iptables -t nat -A POSTROUTING -j MASQUERADE
