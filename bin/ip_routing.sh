aa035bf8044b8507b00a8d0aba864a5b768fe87d
root <root@vps222215.vps.ovh.ca>
2021-02-07 06:40:08 -0500

    Add routing for SSH

--- bin/ip_routing.sh
+++ bin/ip_routing.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -ex
+# Smarthome DOM
 iptables -t nat -A PREROUTING -p tcp --dport 65010 -j DNAT --to-destination 192.168.255.10:80
 iptables -t nat -A PREROUTING -p tcp --dport 64010 -j DNAT --to-destination 192.168.255.10:3080
+iptables -t nat -A PREROUTING -p tcp --dport 63010 -j DNAT --to-destination 192.168.255.10:22
+
+# SSH raspi AGH
+iptables -t nat -A PREROUTING -p tcp --dport 63006 -j DNAT --to-destination 192.168.255.6:22
+
 iptables -t nat -A POSTROUTING -j MASQUERADE
